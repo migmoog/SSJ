3c38c9cc9eb208491193bb3148c4f18e59bb643c
mig-moog <orangej0312@gmail.com>
2020-11-22 13:12:28 -0500

    Added highlighted buttons

--- src/actors/TBGbtns.js
+++ src/actors/TBGbtns.js
@@ -1,21 +1,19 @@
+// TODO add Throw Build Gather Commands
 export default class TBGbtns extends Phaser.GameObjects.Group {
     /**
      * @param {Phaser.Scene} scn 
      */
     constructor(scn) {
         super(scn, [
-            scn.add.sprite(scn.scale.width / 2, 196, 'p1-btn', 0),            
-            scn.add.sprite(scn.scale.width / 2, 196, 'p1-btn', 1),            
-            scn.add.sprite(scn.scale.width / 2, 196, 'p1-btn', 2)            
+            new Btn(scn, scn.scale.width / 2, 196, 'p1-btn', 0),
+            new Btn(scn, scn.scale.width / 2, 196, 'p1-btn', 2),
+            new Btn(scn, scn.scale.width / 2, 196, 'p1-btn', 4)
         ], { maxSize: 3 });
 
         scn.add.tween({
             ease: Phaser.Math.Easing.Quadratic.InOut,
             targets: this.children.entries[0],
             y: 40,
-            onComplete: (twn, tgt) => {
-                tgt[0].setFrame(3);
-            }
         });
         scn.add.tween({
             ease: Phaser.Math.Easing.Quadratic.InOut,
@@ -30,8 +28,44 @@ export default class TBGbtns extends Phaser.GameObjects.Group {
     }
 
     preUpdate(t, dt) {
-        const k = this.scene.keys; 
-        
+        const p = this.scene.input.activePointer;
+
+        super.preUpdate(t, dt);
+    }
+}
+
+class Btn extends Phaser.GameObjects.Sprite {
+    /**
+     * @param {Phaser.Scene} scn 
+     * @param {number} x 
+     * @param {number} y 
+     * @param {string} texture 
+     * @param {number} frame 
+     */
+    constructor(scn, x, y, texture, frame) {
+        super(scn, x, y, texture, frame);
+
+        this.setInteractive();
+        this.on('pointerover', () => {
+            this.setFrame(this.returnHighlight(frame));
+        });
+        this.on('pointerout', () => {
+            this.setFrame(frame);
+        });
+
+        scn.add.existing(this);
+    }
+
+    returnHighlight(frm) {
+        if (frm === 0)
+            return 1
+        else if (frm === 2)
+            return 3
+        else if (frm === 4)
+            return 5
+    }
+
+    preUpdate(t, dt) {
         super.preUpdate(t, dt);
     }
 }
--- src/scenes/Scene.js
+++ src/scenes/Scene.js
@@ -2,27 +2,19 @@ import Family from "../actors/Family.js";
 import TBGbtns from "../actors/TBGbtns.js";
 
 export default class Scene extends Phaser.Scene {
-    keys;
-
     /**@type {Phaser.GameObjects.Sprite} */
     snowPile;
     /**@type {Family} */
     player1;
     /**@type {Family} */
     player2;
-    
+
     /**@type {Phaser.GameObjects.Group} */
     p1Btns;
     /**@type {Phaser.GameObjects.Group} */
     p2Btns;
 
-    //DEBUG
-    canMakeBtns = true;
-
     create() {
-        // CONTROLS
-        this.keys = this.input.keyboard.addKeys('left,right,up,down,z');
-
         this.add.image(0, 0, 'bg').setOrigin(0, 0);
 
         this.player1 = new Family(this, 70, 40, 'p1-', 'cat', 120);
@@ -34,13 +26,7 @@ export default class Scene extends Phaser.Scene {
     }
 
     update(d, dt) {
-        // DEBUG
-        // this.keys.z.on('down', () => {
-        //     if (this.canMakeBtns) {
-        //         this.p1Btns = new TBGbtns(this);
-        //         this.canMakeBtns = false;
-        //     }
-        // });
+
     }
 
     constructor() { super('scene'); }
